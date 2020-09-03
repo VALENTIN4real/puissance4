7df67f84a0f8f9c47f575ddb967a4f043310dd22
Valentin Mairesse <mairesse.valentin.pro@gmail.com>
2020-09-03 11:40:14 +0200

    plateau de jeu

--- src/main.java
+++ src/main.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class main {
+    public static void main(String[] args) {
+        plateau();
+    }
+
+    public static void plateau(){
+        int x = 6;
+        int y = 7;
+
+        String[][] plateau = new String[x][y];
+
+        for(int i = 0; i < x; i++) {
+            for (int z = 0; z < y; z++) {
+                plateau[i][z] = "   |";
+                System.out.print(plateau[i][z]);
+            }
+            System.out.println("");
+        }
+
+        System.out.println(" 1   2   3   4   5   6   7   8");
+    }
 }
